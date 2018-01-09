b238fee915363e36f7ecec17754d6cf84dc4cee3
Cesar <rogercesar778876@gmail.com>
2018-01-09 11:36:24 -0500

    add box art

---
+++ box.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  printf("   +--------------+\n  /|             /|\n / |            / |\n*--+-----------*  |\n|  |           |  |\n|  |           |  |\n|  |pickupbox  |  |\n|  +-----------+--+\n| / by Cesar M.| /\n|/  & Alex S.  |/\n*--------------*\n");
+}
