cea4a20fac0e6687fa8c74021a60ac4cff098a6f
roger778876 <rogercesar778876@gmail.com>
2018-01-25 01:39:41 -0500

    client

---
+++ client.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include "pipe_networking.c"
+#include "startup.c"
+
+int main() {
+  startuptext(0);
+
+
+  int to_server;
+  int from_server;
+  char buffer[BUFFER_SIZE];
+
+  from_server = client_handshake( &to_server );
+  /*
+  while (1) {
+    printf("enter data: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    *strchr(buffer, '\n') = 0;
+    write(to_server, buffer, sizeof(buffer));
+    read(from_server, buffer, sizeof(buffer));
+    printf("received: [%s]\n", buffer);
+  }
+  */
+}
